b251da5395439bf15d0f37b16d2dd4f0b225af40
SponTanious <dani.tanious@hotmail.com>
2018-05-29 12:44:16 +1000

    Remote Calibration talker fixed

--- scripts/laser_angle_talker.py
+++ scripts/laser_angle_talker.py
@@ -9,18 +9,18 @@ import rospkg
 
 def get_transform(laser_frame, parent_frame, x, y, z):
 	t = TransformStamped()
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = parent_frame
-    t.child_frame_id = laser_frame
-    t.transform.translation.x = 0
-    t.transform.translation.y = 0
-    t.transform.translation.z = 0.1
-    q = tf.transformations.quaternion_from_euler(x, y, z)
-    t.transform.rotation.x = q[0]
-    t.transform.rotation.y = q[1]
-    t.transform.rotation.z = q[2]
-    t.transform.rotation.w = q[3]
-    
+	t.header.stamp = rospy.Time.now()
+	t.header.frame_id = parent_frame
+	t.child_frame_id = laser_frame
+	t.transform.translation.x = 0
+	t.transform.translation.y = 0
+	t.transform.translation.z = 0.1
+	q = tf.transformations.quaternion_from_euler(x, y, z)
+	t.transform.rotation.x = q[0]
+	t.transform.rotation.y = q[1]
+	t.transform.rotation.z = q[2]
+	t.transform.rotation.w = q[3]
+
 	return t
 
 # Converts a number from degrees to radians
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     #Initiate Node
     rospy.init_node('tf2_laser_angle_calibrator')
     
-    pub = rospy.Publisher('calibratorTransform', Transform, queue_size=1)
+    pub = rospy.Publisher('calibratorTransform', TransformStamped, queue_size=1)
     
     #Calibration section user prompts
     print "You have entered the LiDAR Calibration tool"
@@ -53,11 +53,11 @@ if __name__ == '__main__':
             pub.publish( get_transform(laser_frame, parent_frame, radians[0], radians[1], radians[2]) )
         elif degrees[0] == 'q':
             if laser == 1:
-                print "\nAdjusting LIDAR 2"
-                laser = 2
-                laser_frame = "laser2"
+				print "\nAdjusting LIDAR 2"
+				laser = 2
+				laser_frame = "laser2"
 				parent_frame = "laser_mount2"
-                current_angle_deg = [0, 0, 0]    
+				current_angle_deg = [0, 0, 0]    
             elif laser == 2:
                 break
             else:
@@ -66,3 +66,5 @@ if __name__ == '__main__':
     print "You have stopped making changes"
     print "Quiting the LiDAR Claibration Tool"
     sleep(1)
+    
+    rospy.spin()
